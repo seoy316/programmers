0188b10b6420252272af603d98c5300094e0d0bb
장서윤 <67985667+seoy316@users.noreply.github.com>
2024-06-01 00:36:58 +0900

    [level 3] Title: 단속카메라, Time: 2.30 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,17 @@
+def solution(routes):
+    answer = 0
+    
+    routes.sort(key = lambda x:x[0])
+    start = routes[0][0]
+    end = routes[0][1]
+    
+    for i in range(1, len(routes)):
+        if start <= routes[i][0] <= end:
+            start = max(start, routes[i][0])
+            end = min(end, routes[i][1])
+            continue
+        else:
+            end = max(end, routes[i][1])
+            answer+=1
+                
+    return answer+1
