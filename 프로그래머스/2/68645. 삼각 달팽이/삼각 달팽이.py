80554fe1885e5c2192895bdbfbd2c52610298335
장서윤 <67985667+seoy316@users.noreply.github.com>
2024-06-25 00:54:26 +0900

    [level 2] Title: 삼각 달팽이, Time: 102.80 ms, Memory: 43.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -0,0 +1,22 @@
+def solution(n):
+    triangle = [[0] * i for i in range(1, n + 1)]
+    
+    x, y = -1, 0
+    num = 1
+    for i in range(n):
+        for j in range(i, n):
+            if i % 3 == 0:  # down
+                x += 1
+            elif i % 3 == 1:  # right
+                y += 1
+            else:  # up
+                x -= 1
+                y -= 1
+            triangle[x][y] = num
+            num += 1
+    
+    answer = []
+    for row in triangle:
+        answer.extend(row)
+    
+    return answer
